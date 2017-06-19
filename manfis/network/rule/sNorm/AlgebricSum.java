2fbef7d4db804fb064d97cf802a3d36572795557
Ar-nas <arifrakhmatn@gmail.com>
2017-06-19 09:43:30 +0700

    Add files via upload

---
+++ manfis/network/rule/sNorm/AlgebricSum.java
@@ -0,0 +1,33 @@
+﻿package com.ar.nas.manfis.network.rule.sNorm;
+
+/**
+ *
+ * @author arif
+ */
+public class AlgebricSum extends SNorm {
+    
+    private final int nInput;
+    
+    public AlgebricSum(int nInput) {
+        this.nInput = nInput;
+    }
+    
+    @Override
+    public double computeNorm(double μ[]) {
+        double sum = 0;
+        for (int i = 0; i < nInput; i++) {
+            sum = sum + μ[i];
+        }
+        return sum;
+    }
+    
+    @Override
+    public double[] calculateError(double error, double μ[]) {
+        double E[] = new double[nInput];
+        for (int i = 0; i < nInput; i++) {
+            E[i] = 1.0;
+        }
+        return E;
+    }
+    
+}
