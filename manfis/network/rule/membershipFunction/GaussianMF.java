fb72f25338683ae13049c2a03006a5f63d36d697
Ar-nas <arifrakhmatn@gmail.com>
2017-06-19 09:38:31 +0700

    Update GaussianMF.java

--- manfis/network/rule/membershipFunction/GaussianMF.java
+++ manfis/network/rule/membershipFunction/GaussianMF.java
@@ -19,7 +19,6 @@ public class GaussianMF extends MembershipFunction {
     
     @Override
     public double compute(double x) {
-        //μ = Math.exp(Math.pow(x-a,2)/-(2*b));
         μ = Math.exp(-b*Math.pow(a-x,2));
         return μ;
     }
@@ -28,9 +27,6 @@ public class GaussianMF extends MembershipFunction {
     public void correctError(double error, double x, double η, double m) {
         α = (1-m) * α + η * x * error * 2*b*(a-x)*μ;
         β = (1-m) * β + η * x * error * Math.pow(a-x,2)*-μ;
-        //System.out.println(error);        
-        //System.out.println(error * -2*b*(a-x)*μ);
-        //System.out.println(error * Math.pow(a-x,2)*-μ);
         a = a + α;
         b = b + β;
     }
