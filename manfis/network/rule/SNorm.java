e6a38220c02f9b66697dd0a81fb28215f566503c
Ar-nas <arifrakhmatn@gmail.com>
2017-06-19 09:40:45 +0700

    Add files via upload

---
+++ manfis/network/rule/SNorm.java
@@ -0,0 +1,13 @@
+﻿package com.ar.nas.manfis.network.rule.sNorm;
+
+/**
+ *
+ * @author arif
+ */
+public abstract class SNorm {
+    
+    public abstract double computeNorm(double μ[]);
+    
+    public abstract double[] calculateError(double error, double μ[]);
+    
+}
