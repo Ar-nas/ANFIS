2abfed23884a090532c4ed2fd913dc43fd3151f7
Ar-nas <arifrakhmatn@gmail.com>
2017-06-19 09:45:15 +0700

    Add files via upload

---
+++ manfis/network/rule/tNorm/BasicTNorm.java
@@ -0,0 +1,59 @@
+﻿package com.ar.nas.manfis.network.rule.tNorm;
+
+/**
+ *
+ * @author ARNAS_
+ */
+public class BasicTNorm extends TNorm {
+    
+    private final int nInput;
+    
+    public BasicTNorm(int nInput) {
+        this.nInput = nInput;
+    }
+	
+    @Override
+    public double computeNorm(double μ[]) {
+        double min = Double.MAX_VALUE;
+        for (double μ1 : μ) {
+            if (μ1 < min)
+                min = μ1;
+        }
+        return min;
+    }
+    
+    @Override
+    public double[] calculateError(double error, double μ[]) {
+        double E[] = new double[nInput];
+        
+        double min = μ[0];
+        int x = 0;
+        for (int i = 1; i < nInput; i++) {
+            if (μ[i] < min) {
+                min = μ[i];
+                x = i;
+            }
+        }
+        
+        E[x] = error;
+        return E;
+    }
+    
+    public double computeNorm1(double μ[]) {
+        double sum = 0;
+        for (double μ1 : μ) {
+            sum += μ1;
+        }
+        return sum;
+    }
+    
+    public double[] calculateError1(double error, double μ[]) {
+        double E[] = new double[nInput];
+        
+        for (int i = 0; i < nInput; i++) {
+            E[i] = error;
+        }
+        
+        return E;
+    }
+}
---
+++ manfis/network/rule/tNorm/EinsteinProduct.java
@@ -0,0 +1,47 @@
+﻿package com.ar.nas.manfis.network.rule.tNorm;
+
+/**
+ *
+ * @author ARNAS_
+ */
+public class EinsteinProduct extends TNorm {
+    
+    private final int nInput;
+    
+    public EinsteinProduct(int nInput) {
+        this.nInput = nInput;
+    }
+    
+    @Override
+    public double computeNorm(double μ[]) {
+        double sum = 0;
+        double product = 1;
+        for (int i = 0; i < nInput; i++) {
+            sum = sum + μ[i];
+            product = product * μ[i];
+        }
+        return product / (2 - sum + product);
+    }
+    
+    @Override
+    public double[] calculateError(double error, double μ[]) {
+        double E[] = new double[nInput];
+        
+        for (int i = 0; i < nInput; i++) {
+            double sum = 0;
+            double product = 1;
+            for (int j = 0; j < nInput; j++) {
+                if (i != j) {
+                    sum = sum + μ[j];
+                    product = product * μ[j];
+                }
+            }
+            E[i] = error * - product * (sum - 2) / Math.pow((2 - (μ[i] + sum - μ[i] * product)), 2);
+            //System.out.println("error "+error);
+            //System.out.println("E"+i+" "+E[i]);
+        }
+        
+        return E;
+    }
+    
+}
